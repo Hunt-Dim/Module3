e4399861367a0ccb7e01803f35b6d0c68efcd291
Hunt-Dim <166534559+Hunt-Dim@users.noreply.github.com>
2024-06-17 22:51:32 +0700

    Add files via upload

---
+++ HomeWork_2_Mod3.py
@@ -0,0 +1,13 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+
+print_params()
+print_params(b=25)
+print_params(c=[1, 2, 3])
+values_list = [500, 'Привет!', True]
+values_list_2 = [54.32, 'Строка']
+values_dict = {'a': 2.258, 'b': False, 'c': 'Словарь'}
+print_params(*values_list)
+print_params(*values_list_2, 42)
+print_params(**values_dict)
